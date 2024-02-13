99e4783995cb042d60ca05a4cd14be50b7827a45
coolcool <159146271+limseewon@users.noreply.github.com>
2024-02-13 12:19:53 +0900

    Update index.php

--- simple_bbs/index.php
+++ simple_bbs/index.php
@@ -1,5 +1,8 @@
 <?php
   require_once('config.php');
+
+  $sql = "SELECT * FROM bbs";
+  $result = mysqli_query($mysqli, $sql);
 ?>
 <!DOCTYPE html>
 <html lang="ko">
@@ -11,9 +14,9 @@
 <body>
   <h1>Simple BBS</h1>
   <ul>
-    <li><a href="">글제목1</a><a href="">수정</a><a href="">삭제</a></li>
-    <li><a href="">글제목1</a><a href="">수정</a><a href="">삭제</a></li>
-    <li><a href="">글제목1</a><a href="">수정</a><a href="">삭제</a></li>
+
+    <!-- <li><a href="">글제목1</a><a href="">수정</a><a href="">삭제</a></li> -->
+
   </ul>
   <p>
   <a href="write.php">글쓰기</a> 
